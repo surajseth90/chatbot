f83f4e16dbd0b1f443408ee58f940a7b309dcad3
surajseth90 <surajseth90@gmail.com>
2023-10-11 16:52:02 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 import Chatbot from "./Chatbot";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { setHtmlData } from "./action";
 
 function App() {
   const htmlData = useSelector((store) => store.htmlData);
+  const dispatch = useDispatch();
+
+  const clearClickHandler = () => {
+    dispatch(setHtmlData([]));
+  };
 
   return (
     <div className="app">
       <div className="left-side">
         <Chatbot />
+        <button className="clearbtn" onClick={clearClickHandler}>
+          Clear
+        </button>
       </div>
       <div className="rightside">
-        {htmlData.length>0 ? (
-          <ul dangerouslySetInnerHTML={{ __html: htmlData }} />
+        {htmlData.length > 0 ? (
+          <ul>
+            {htmlData.map((data) => {
+              return <li dangerouslySetInnerHTML={{ __html: data }}></li>;
+            })}
+          </ul>
         ) : (
           <p>Ask your question to bot</p>
         )}
